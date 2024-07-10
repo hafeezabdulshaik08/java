91430d298977b267460cad740e03f5245d23a280
hafeezabdulshaik08 <168513325+hafeezabdulshaik08@users.noreply.github.com>
2024-07-10 12:31:18 +0530

    Create greatestoftwonumbers.java

---
+++ greatestoftwonumbers.java
@@ -0,0 +1,9 @@
+public class greatest{
+	public static void main(String[]args){
+		int a=5 ,b=10;
+		if(a<b)
+			System.out.println("b is greater");
+		else
+			System.out.println("a is greater");	
+	}
+}
