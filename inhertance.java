f9d632af1f243a1ef13cf2694986a09737d3b85c
SHAIK ABDUL HAFEEZ <168513325+hafeezabdulshaik08@users.noreply.github.com>
2024-09-18 12:10:41 +0530

    Update inhertance.java

--- inhertance.java
+++ inhertance.java
@@ -1,4 +1,4 @@
-class vehicle{
+1.class vehicle{
 	void start(){
 		System.out.println("Vehicle started");
 	}
@@ -16,3 +16,41 @@ public class Inheritance1{
 		c.start();
 	}
 }							
+
+
+
+2.class person{
+	String name;
+	int age;
+		
+	void display(){
+		System.out.println("name:"+name);
+		System.out.println("age"+age);
+	}
+}
+class employee extends person	{
+	double salary;
+	employee(String a,int b,double c){
+		name=a;
+		age=b;	
+		salary=c;
+	}
+	void display(){
+		System.out.println("name:"+name);
+		System.out.println("age"+age);
+		System.out.println("salary"+salary);
+		
+	}
+}
+public class Inheritance2{
+	public static void main(String[] args){
+		person p=new person();
+		p.name="vamsi";
+		p.age=18;
+		p.display();
+		employee e=new employee("abdul",18,50000);		
+		e.display();
+	}
+}		
+				
+		
